da501211dadff33f0fcd3c35d0f768ddccc47494
madhuri <madhuri.km46@gmail.com>
2023-10-26 12:47:09 -0400

    on-episode-selection

--- src/components/EpisodesList.tsx
+++ src/components/EpisodesList.tsx
@@ -79,17 +79,20 @@ const EpisodeList: React.FC<EpisodeListProps> = (props) => {
       >
         {episodes.map((episode) => (
           <li className="pb-0 text-left" key={episode.id}>
-              <button
-                onClick={() => {
-                  props.onChange && props.onChange(getCharacterIds(episode), episode);
-                }}
-                type="button"
-                className={`py-2.5 w-full max-w-md px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ${
-                  episode.id === props.selectedEpisode?.id ? 'bg-blue-700 hover:bg-blue-800 text-white hover:text-white' : ''
-                }`}
-              >
-                {episode.name}
-              </button>
+            <button
+              onClick={() => {
+                props.onChange &&
+                  props.onChange(getCharacterIds(episode), episode);
+              }}
+              type="button"
+              className={`py-2.5 w-full max-w-md px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none rounded-lg border border-gray-200  hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ${
+                episode.id === props.selectedEpisode?.id
+                  ? "bg-blue-700 hover:bg-blue-800 text-white hover:text-white"
+                  : "bg-white hover:bg-gray-100 hover:bg-gray-100"
+              }`}
+            >
+              {episode.name}
+            </button>
           </li>
         ))}
       </ul>
